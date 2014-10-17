d7864a10c71c646ca64ea300e882dc35964b4bb5
TonySymon <tonysymon@outlook.com>
2014-10-17 22:22:03 +0800

    Init the project.

---
+++ scripts/app.js
@@ -0,0 +1,20 @@
+﻿
+
+$(document).ready(function () {
+    inputElement = $('#inputword');
+    sourceElement = $('#sourceword');
+    console.log("ready");
+    var text = sourceElement.text();
+    var i = 0;
+    inputElement.keydown(function (data, fn) {
+        var c = String.fromCharCode(data.keyCode);
+        console.log(c);
+        console.log(text[i]);
+        if(c!=text[i])
+            inputElement.css("background-color", "#D6D6FF");
+        else
+            inputElement.css("background-color", "#FFFFFF");
+        i++;
+    });
+});
+
