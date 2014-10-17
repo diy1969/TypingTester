0d489080911ab384e57739feade82aa7a207e5a5
duter <5052909@qq.com>
2014-10-18 01:41:27 +0800

    添加了一些背景样式。

--- scripts/app.js
+++ scripts/app.js
@@ -6,15 +6,31 @@ $(document).ready(function () {
     console.log("ready");
     var text = sourceElement.text();
     var i = 0;
+    var cur = $('#currentword');
+    var str = cur.text().toLowerCase();
+    var tmp = '';
     inputElement.keydown(function (data, fn) {
-        var c = String.fromCharCode(data.keyCode);
-        console.log(c);
-        console.log(text[i]);
-        if(c!=text[i])
-            inputElement.css("background-color", "#D6D6FF");
-        else
-            inputElement.css("background-color", "#FFFFFF");
-        i++;
+        var k = String.fromCharCode(data.keyCode).toLowerCase();
+        //console.log(data.keyCode);
+        if (data.keyCode == 8) {
+            i--;
+            return;
+        }
+        if (k == str[i]) {
+            tmp = tmp + "<span class='correct'>" + str[i] + "</span>";
+            i++;
+            cur.html(tmp + str.substring(i, str.length));
+        }
+        if (data.keyCode == 32) {
+            i = 0;
+            cur.removeAttr('id');
+            cur.addClass('correctword');
+            cur.html(str);
+            cur = cur.next();
+            cur.attr('id', 'currentword');
+            str = cur.text().toLowerCase();
+            tmp = '';
+        }
     });
 });
 
